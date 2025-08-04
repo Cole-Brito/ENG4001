e2fa1583b8bc97c67c7e7e5df0814864dea934ed
Bivin Job <bivinjob@gmail.com>
2025-08-04 05:12:44 -0400

    commented out dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -59,6 +59,33 @@ class MyApp extends StatelessWidget {
           secondary: Color(0xFF10138A),
           tertiary: Colors.black,
         ),
+
+        // Uncomment the following lines to enable dark theme
+
+        //    darkTheme: ThemeData(
+        //     useMaterial3: true,
+        //     brightness: Brightness.dark,
+        //     scaffoldBackgroundColor: Colors.black,
+        //     fontFamily: 'GlacialIndifference',
+        //     textTheme: const TextTheme(
+        //       headlineSmall: TextStyle(fontFamily: 'BebasNeue', fontSize: 22, color: Colors.white),
+        //       titleLarge: TextStyle(fontFamily: 'bebasneuecyrillic', fontSize: 24, color: Colors.white),
+        //       titleMedium: TextStyle(fontFamily: 'bebasneuecyrillic', fontSize: 20, color: Colors.white),
+        //       bodyLarge: TextStyle(fontFamily: 'GlacialIndifference', fontSize: 16, color: Colors.white),
+        //       bodySmall: TextStyle(fontFamily: 'GlacialIndifference', fontSize: 14, color: Colors.white),
+        //     ),
+        //     colorScheme: ColorScheme.dark(
+        //       primary: Color(0xFF10138A), // ROS Blue
+        //       onPrimary: Colors.white,
+        //       onSurface: Colors.white,
+        //       onSurfaceVariant: Colors.white70,
+        //       secondary: Color(0xFF10138A),
+        //       tertiary: Colors.white,
+        //     ),
+        //     appBarTheme: const AppBarTheme(centerTitle: true),
+        //   ),
+
+        // themeMode: ThemeMode.system,
         appBarTheme: const AppBarTheme(centerTitle: true),
       ),
       home: const LoginScreen(),
